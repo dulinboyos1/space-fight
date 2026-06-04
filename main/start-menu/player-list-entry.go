8228598d65aa67c324f698da03026bd64996a170
dulinboyos1 <joew8277@gmail.com>
2026-06-04 11:00:26 +0100

    Finished player adding, working on reactive menu system, about to write the update navigator function in start-menu.script

---
+++ main/start-menu/player-list-entry.go
@@ -0,0 +1,32 @@
+embedded_components {
+  id: "remove-button"
+  type: "sprite"
+  data: "default_animation: \"Remove Button\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/start-menu/start-menu.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 1301.0
+  }
+}
+embedded_components {
+  id: "label"
+  type: "label"
+  data: "size {\n"
+  "  x: 128.0\n"
+  "  y: 32.0\n"
+  "}\n"
+  "pivot: PIVOT_W\n"
+  "text: \" = Player [_]\"\n"
+  "font: \"/builtins/fonts/default.font\"\n"
+  "material: \"/builtins/fonts/label-df.material\"\n"
+  ""
+  scale {
+    x: 5.5
+    y: 5.5
+    z: 5.5
+  }
+}
