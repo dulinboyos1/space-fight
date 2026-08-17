963bf7a8b0059b8d8e280ee539f7e00bdcf3db75
dulinboyos1 <joew8277@gmail.com>
2026-08-17 11:02:42 +0100

    The extra files I forgot

---
+++ main/gameplay/player/trailNode.go
@@ -0,0 +1,11 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"White Square\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/common.atlas\"\n"
+  "}\n"
+  ""
+}
